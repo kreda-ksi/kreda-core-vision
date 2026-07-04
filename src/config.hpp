e18c77cce8395189a1e8e3c7bdc1bb3a9baace98
nihiL7331 <pujanekp6@gmail.com>
2026-07-05 00:19:46 +0200

    feat: keyframe extraction state machine

--- src/config.hpp
+++ src/config.hpp
@@ -12,8 +12,19 @@ inline constexpr unsigned int POINTS_CNT = COLUMN_CNT * 4;
 inline constexpr float OUT_WID = 1280.0f;
 inline constexpr float OUT_HEI = 720.0f;
 inline constexpr unsigned int MAX_RETRIES = 30;
+
 inline constexpr bool SHOW_RAW = true;
+
+inline constexpr int MOTION_THRESH_INTENSITY =
+    30; // px intens diff to count as 'changed'
+inline constexpr int MOTION_TRIGGER_PXS =
+    5000; // how many pxs must change to trigger movement
+inline constexpr int STATE_CHANGE_PXS =
+    15000; // how many pxs must differ from the last saved board
+inline constexpr int STILL_COOLDOWN =
+    30; // how many frames of stillness required to capture
 
 // i/o
 inline constexpr const char *CALIB_FILE = "calibration.xml";
+inline constexpr const char *OUT_DIR = "staging";
 } // namespace kreda
--- src/ingestion.cpp
+++ src/ingestion.cpp
@@ -1,9 +1,9 @@
 #include "ingestion.hpp"
 #include "config.hpp"
 #include <chrono>
+#include <filesystem>
 #include <format>
 #include <iostream>
-#include <thread>
 
 namespace kreda {
 
@@ -23,13 +23,71 @@ static cv::Mat enhanceChalkboard(const cv::Mat &raw_board,
     return final;
 }
 
+// evaluates if a frame should be saved based on verification.
+// the verification process is as follows:
+// 1. on image difference (motion detection) track goes to an 'alert' state,
+// 2. if after the 'alert' state cools down there's a visible difference on the
+// chalkboard, then proceed to save the frame. otherwise, just continue.
+static void evaluateAndExtract(const cv::Mat &curr_frame, TrackState &state,
+                               unsigned int track_id,
+                               const std::string &out_dir) {
+    if (state.prev_frame.empty()) { // inital state
+        state.prev_frame = curr_frame.clone();
+        state.last_saved_frame = curr_frame.clone();
+        return;
+    }
+
+    cv::Mat diff1, thresh1;
+    cv::absdiff(curr_frame, state.prev_frame, diff1);
+    cv::threshold(diff1, thresh1, MOTION_THRESH_INTENSITY, 255,
+                  cv::THRESH_BINARY);
+
+    if (cv::countNonZero(thresh1) > MOTION_TRIGGER_PXS) {
+        state.is_moving = true;
+        state.still_cnt = 0;
+    } else if (state.is_moving) {
+        state.still_cnt++;
+
+        if (state.still_cnt >= STILL_COOLDOWN) {
+            cv::Mat diff2, thresh2;
+            cv::absdiff(curr_frame, state.last_saved_frame, diff2);
+            cv::threshold(diff2, thresh2, MOTION_THRESH_INTENSITY, 255,
+                          cv::THRESH_BINARY);
+
+            if (cv::countNonZero(thresh2) > STATE_CHANGE_PXS) {
+                auto timestamp =
+                    std::chrono::duration_cast<std::chrono::seconds>(
+                        std::chrono::system_clock::now().time_since_epoch())
+                        .count();
+
+                std::string filename = std::format(
+                    "{}/track_{}_{}.png", out_dir, track_id, timestamp);
+                cv::imwrite(filename, curr_frame);
+
+                std::cout << std::format("Track {} updated. Saved to {}.",
+                                         track_id, filename)
+                          << std::endl;
+
+                state.last_saved_frame = curr_frame.clone();
+            }
+
+            state.is_moving = false;
+            state.still_cnt = 0;
+        }
+    }
+
+    state.prev_frame = curr_frame.clone();
+}
+
 void runIngestionLoop(cv::VideoCapture &cap, const std::string &rtsp_url,
                       const std::array<cv::Mat, COLUMN_CNT> &warp_matrices) {
+    std::filesystem::create_directory(OUT_DIR);
+
     cv::Mat frame;
-    std::array<cv::Mat, COLUMN_CNT> dewarped_boards;
     int retry_cnt = 0;
 
     cv::Ptr<cv::CLAHE> clahe = cv::createCLAHE(2.0);
+    std::array<TrackState, COLUMN_CNT> track_states; // holds state for all cols
 
     while (true) {
         bool succ = cap.read(frame);
@@ -64,17 +122,20 @@ void runIngestionLoop(cv::VideoCapture &cap, const std::string &rtsp_url,
         retry_cnt = 0;
 
         for (unsigned int i{}; i < COLUMN_CNT; ++i) {
+            cv::Mat dewarped;
+
             // homography
-            cv::warpPerspective(frame, dewarped_boards[i], warp_matrices[i],
+            cv::warpPerspective(frame, dewarped, warp_matrices[i],
                                 cv::Size(OUT_WID, OUT_HEI));
 
-            cv::Mat final_board = enhanceChalkboard(dewarped_boards[i], clahe);
+            // pixel math
+            cv::Mat final = enhanceChalkboard(dewarped, clahe);
 
             // debug display
             if (SHOW_RAW)
                 cv::imshow(std::format("KREDA column {} (raw)", i + 1),
-                           dewarped_boards[i]);
-            cv::imshow(std::format("KREDA column {}", i + 1), final_board);
+                           dewarped);
+            cv::imshow(std::format("KREDA column {}", i + 1), final);
         }
         cv::pollKey();
     }
