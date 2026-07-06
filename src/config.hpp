2fa85281205b218290917156b9705afa77cd36d7
nihiL7331 <pujanekp6@gmail.com>
2026-07-06 02:20:21 +0200

    chore: config update

--- src/config.hpp
+++ src/config.hpp
@@ -7,7 +7,7 @@ inline constexpr int EARG = 1;
 inline constexpr int ERTSP = 2;
 inline constexpr int ECAL = 3;
 
-// config
+// vision
 inline constexpr unsigned int COLUMN_CNT = 2;
 inline constexpr unsigned int POINTS_CNT = COLUMN_CNT * 4;
 inline constexpr float OUT_WID = 1280.0f;
@@ -16,7 +16,7 @@ inline constexpr int TOTAL_AREA =
     OUT_WID * OUT_HEI; // only used internally to calc frame area% consts
 inline constexpr unsigned int MAX_RETRIES = 30;
 
-inline constexpr bool SHOW_RAW = true;
+inline constexpr bool SHOW_RAW = false;
 inline constexpr bool LOG_ENABLED = true;
 inline constexpr bool SHOW_GUI = true;
 
@@ -26,7 +26,7 @@ inline constexpr int CONTENT_THRESH_INTENSITY =
     MOTION_THRESH_INTENSITY; // kept as separate for potential fine-tuning
 inline constexpr int MOTION_TRIGGER_PXS =
     1000; // how many pxs must change to trigger movement
-inline constexpr int SLIDE_TRIGGER_PXS = static_cast<int>(TOTAL_AREA * 0.05);
+inline constexpr int SLIDE_TRIGGER_PXS = static_cast<int>(TOTAL_AREA * 0.2);
 inline constexpr int MAX_STROKE_COMP_AREA = static_cast<int>(TOTAL_AREA * 0.02);
 inline constexpr unsigned int MOTION_HIST_FRAMES = 5;
 inline constexpr int STATE_CHANGE_PXS =
