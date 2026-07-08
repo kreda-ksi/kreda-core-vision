4f9c2c6ad8a8f2cbd015ee605cbb694201065f21
nihiL7331 <pujanekp6@gmail.com>
2026-07-08 14:50:20 +0200

    feat: masking human test

--- tests/test_masking.cpp
+++ tests/test_masking.cpp
@@ -9,3 +9,9 @@ TEST_CASE("masking: small blob counts as chalk") {
     cv::rectangle(mask, {100, 100}, {109, 109}, cv::Scalar(255), cv::FILLED);
     CHECK(countChalkPixels(mask, 1000) == 100);
 }
+
+TEST_CASE("masking: body-sized blob is excluded") {
+    cv::Mat mask = cv::Mat::zeros(720, 1280, CV_8U);
+    cv::rectangle(mask, {100, 100}, {299, 109}, cv::Scalar(255), cv::FILLED);
+    CHECK(countChalkPixels(mask, 1000) == 0);
+}
